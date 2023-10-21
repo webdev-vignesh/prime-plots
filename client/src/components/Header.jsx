5f7bbddb40a51aee6b54ebb8593fce2d4fd15ce9
vigmururg <vigmurug@gmail.com>
2023-10-21 14:24:29 +0530

    chore: app rename

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
         <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
             <Link to="/">
                 <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-                    <span className="text-slate-700">Qu</span>
-                    <span className="text-teal-800">Estate</span>
+                    <span className="text-slate-600">Prime</span>
+                    <span className="text-teal-800">Plots</span>
                 </h1>
             </Link>
             <form className="bg-slate-100 p-2 rounded-lg flex items-center">
