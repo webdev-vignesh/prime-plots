cfec6ff2c4e94991637021018d87e2ceec0acfaf
vigmururg <vigmurug@gmail.com>
2023-10-28 20:07:10 +0530

    get listing api

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -22,10 +22,60 @@ const userSlie = createSlice({
         signInFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
-        }
+        },
+        updateUSerStart: (state) => {
+            state.loading = true;
+        },
+        updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+        deleteUserStart: (state) => {
+            state.loading = true;
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+        signOutUserStart: (state) => {
+            state.loading = true;
+        },
+        signOutUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        signOutUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+
     }
 });
 
-export const { signInStart, signInSuccess, signInFailure} = userSlie.actions;
+export const { 
+    signInStart, 
+    signInSuccess, 
+    signInFailure, 
+    updateUSerStart, 
+    updateUserSuccess, 
+    updateUserFailure,
+    deleteUserStart,
+    deleteUserSuccess,
+    deleteUserFailure,
+    signOutUserStart,
+    signOutUserSuccess,
+    signOutUserFailure
+} = userSlie.actions;
 
 export default userSlie.reducer;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, updateUser, deleteUser, getUserListings } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.post("/update/:id", verifyToken, updateUser ) // the endpoint will first go through the "verifyToken" middleware and then if all conditions are passed it'll pass to next controller "updateUser"
+router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings )
 
 export default router;
