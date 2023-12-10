19aef096c1bcd9610b7ca4a6c5646240dd4d4298
vigmururg <vigmurug@gmail.com>
2023-12-10 14:56:17 +0530

    updated listing, user routes

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing, getListing, getListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing); // verify the user and then allow to create listing
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
