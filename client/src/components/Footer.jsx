a8e5684eb649824c8b24b3b615d7ec3f48a7f5bb
vigmururg <vigmurug@gmail.com>
2023-12-10 14:40:54 +0530

    fix: header, footer updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { BsFacebook, BsYoutube } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div className="relative w-full bottom-0 px-16 pt-6 flex flex-col bg-teal-700 text-white">
+    <div className="relative w-full mt-24 bottom-0 px-16 pt-6 flex flex-col bg-teal-700 text-white">
       <div className="flex flex-col sm:flex-row gap-4 justify-evenly">
         <div className="space-y-4">
           <h1 className="font-semibold underline">About Us</h1>
@@ -12,8 +12,8 @@ const Footer = () => {
             <span className="text-teal-400">Plots</span>
           </h1>
           <div>
-            <p className="break-words max-w-md">Prime Plots is a premium Plots/ Villa provider
-            and the largest platform for buyers and sellers
+            <p className="break-words max-w-md">Prime Plots is a premium Plots or Villa provider,
+            the largest platform for buyers and sellers
             of property to connect in a transparent manner.</p>
           </div>
         </div>
@@ -21,7 +21,7 @@ const Footer = () => {
           <h1 className="font-semibold underline">Contact Us</h1>
           <div className="">
             <p>No. 4, Richard Street,</p>
-            <p>Prime Plateau,</p>
+            <p>12th Lane, Grin Plateau,</p>
             <p>Bristol, UK</p>
           </div>
           <div className="flex items-center gap-4">
@@ -31,12 +31,12 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <h1 className="underline">Subscribe to our newsletter!</h1>
-          <div className="flex gap-4 mt-4">
+          <h1 className="underline">Subscribe to our newsletter</h1>
+          <div className="flex flex-wrap gap-4 mt-4">
             <input type="email" placeholder="Email" className="p-1 focus:outline-none rounded-lg" />
             <button type="submit" className="p-1 bg-white text-black rounded-lg">Submit</button>
           </div>
-          <p className="mt-2">Receive weekly real-estate insights</p>
+          <p className="mt-2">Receive weekly real-estate insights!</p>
         </div>
       </div>
       <div className="text-center mt-4 mb-2">
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,31 @@
 import {FaSearch} from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+
+    const navigate = useNavigate();
+
+    const { currentUser } = useSelector((state) => state.user);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set("searchTerm", searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`); 
+    }
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get("searchTerm");
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search])
+
   return (
     <header className="bg-teal-400 shadow-md">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
@@ -11,13 +35,17 @@ const Header = () => {
                     <span className="text-teal-800">Plots</span>
                 </h1>
             </Link>
-            <form className="bg-slate-100 p-2 rounded-lg flex items-center">
+            <form onSubmit={handleSubmit} className="bg-slate-100 p-2 rounded-lg flex items-center">
                 <input 
                     type="text"
                     placeholder="Search..."
                     className="bg-transparent focus:outline-none w-24 sm:w-64"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
-                <FaSearch className="text-teal-800" />
+                <button onClick={handleSubmit}>
+                    <FaSearch className="text-teal-800" />
+                </button>
             </form>
             <ul className="flex items-center gap-4">
                 <Link to="/">
@@ -26,9 +54,22 @@ const Header = () => {
                 <Link to="/about">
                     <li className="hidden sm:inline text-teal-800 font-semibold hover:underline hover:cursor-pointer">About</li>
                 </Link>
-                <Link to="/sign-in">
-                    <li className="text-white hover:cursor-pointer hover:bg-teal-700 bg-teal-800 px-2 py-1 rounded-md">Sign In</li>
+                <Link to="/profile">
+                {
+                    currentUser 
+                    ?  (
+                        <img 
+                            className="rounded-full h-7 w-7 object-cover" 
+                            src={currentUser.avatar} 
+                            alt="profile" 
+                        />
+                    )
+                    :  <li className="text-white hover:cursor-pointer hover:bg-teal-700 bg-teal-800 px-2 py-1 rounded-md">
+                            Sign In
+                        </li>
+                }
                 </Link>
+
             </ul>
         </div>
     </header>
